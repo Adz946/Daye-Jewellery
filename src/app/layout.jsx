5e37ed2d6b253ca1ede4c42c89dd594d0b6dc1d3
Adam Saleh <adsaleh946@gmail.com>
2025-09-20 14:56:13 +1000

    added 'suspense' due to vercel errors

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Montserrat, Sofia_Sans } from "next/font/google";
 import "../styles/globals.css";
-
+import { Suspense } from "react";
 import { CartProvider } from "@/contexts/CartContext";
 import { FilterProvider } from "@/contexts/FilterContext";
 import { WishlistProvider } from "@/contexts/WishlistContext";
@@ -19,18 +19,26 @@ export const metadata = {
 	description: "Handcrafted, everyday pieces designed for effortless style. Unique, modern, and made to last — find jewellery that tells your story."
 };
 
+function Providers({ children }) {
+    return (
+        <CartProvider>
+            <WishlistProvider>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <FilterProvider>
+						<NavController />
+                        {children}
+                    </FilterProvider>
+                </Suspense>
+            </WishlistProvider>
+        </CartProvider>
+    );
+}
+
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<body className={`${montserrat.variable} ${sofiaSans.variable} antialiased`}>
-				<CartProvider>
-					<FilterProvider>
-						<WishlistProvider>
-							<NavController />
-							{children}
-						</WishlistProvider>
-					</FilterProvider>
-				</CartProvider>
+				<Providers> {children} </Providers>
 			</body>
 		</html>
 	);
