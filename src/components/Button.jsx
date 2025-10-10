139a667fc15d98048561ba78f27dbb7b84273aaf
Adam Saleh <adsaleh946@gmail.com>
2025-10-10 21:27:19 +1100

    DB Completion - 2
    Miiiiiiiight have not gotten to users yet, whatever...
    
    Took me 2 days to properly sift through the checkout stuff, meaning checkout --> payments (INCOMPLETE) --> orders
    
    So that should properly do checkout and orders (which can be accessed via the profile page too) BUT payments still needs to incorporate stripe, because that's a thing. Will do so next.
    
    So far, orders are created and gotten correctly, no issues there. the API seems to be handling this well and a new DB View was created to handle ORDER complexity (vw_OrderDetails), which is obtaining the correct data.
    
    As a side - the PhoneInput component is now fully functional, which is neat.
    
    As another side - validation handles normalization as well now, which is also pretty neat.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import { Loader2 } from 'lucide-react';
 export function Button({wd = "w-full", text, onClick = null, disabled = null, loading = false, className = ""}) {
     return ( 
         <button onClick={onClick} disabled={disabled || loading} className={`${wd} py-4 text-xl text-light bg-dark 
-            rounded-sm font-title  animate hover:bg-black hover:scale-110 hover:drop-shadow-2xl disabled:opacity-75 
+            rounded-sm font-title  animate hover:bg-black hover:scale-95 hover:drop-shadow-2xl disabled:opacity-75 
             ${className}`}>
                 {loading ? (
                     <div className="flex items-center justify-center gap-2">
